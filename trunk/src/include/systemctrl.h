667e84127eed3632c2de83c760f523f57254066e
Rinnegatamante <rinnegatamante@gmail.com>
2011-07-10 09:55:18 +0000

    Updating systemctrl module.

--- trunk/src/include/systemctrl.h
+++ trunk/src/include/systemctrl.h
@@ -6,6 +6,15 @@
 #include <psploadexec_kernel.h>
 #include <pspinit.h>
 
+enum BootLoadFlags
+{
+    BOOTLOAD_VSH = 1,
+    BOOTLOAD_GAME = 2,
+    BOOTLOAD_UPDATER = 4,
+    BOOTLOAD_POPS = 8,
+    BOOTLOAD_UMDEMU = 64, /* for original NP9660 */
+};
+
 /**
  * Restart the vsh.
  *
@@ -145,7 +154,7 @@ int sctrlKernelSetDevkitVersion(int version);
  * @returns 1 if we are in SE-C or later, 0 if we are in HEN-D or later,
  * and < 0 (a kernel error code) in any other case
 */
-int	sctrlHENIsSE();
+int    sctrlHENIsSE();
 
 /**
  * Checks if we are in Devhook.
@@ -153,7 +162,7 @@ int	sctrlHENIsSE();
  * @returns 1 if we are in SE-C/HEN-D for devhook  or later, 0 if we are in normal SE-C/HEN-D or later,
  * and < 0 (a kernel error code) in any other case
 */
-int	sctrlHENIsDevhook();
+int    sctrlHENIsDevhook();
 
 /**
  * Gets the HEN version
@@ -240,26 +249,26 @@ typedef int (* STMOD_HANDLER)(SceModule2 *);
  *
  * void somepointofmycode()
  * {
- *		previous = sctrlHENSetStartModuleHandler(OnModuleStart);
+ *        previous = sctrlHENSetStartModuleHandler(OnModuleStart);
  * }
  *
  * int OnModuleStart(SceModule2 *mod)
  * {
- *		if (strcmp(mod->modname, "vsh_module") == 0)
- *		{
- *			// Do something with vsh module here
- *		}
+ *        if (strcmp(mod->modname, "vsh_module") == 0)
+ *        {
+ *            // Do something with vsh module here
+ *        }
  *
- *		if (!previous)
- *			return 0;
+ *        if (!previous)
+ *            return 0;
  *
- *		// Call previous handler
+ *        // Call previous handler
  *
- *		return previous(mod);
+ *        return previous(mod);
  * }
  *
  * @Note2: The above example should be compiled with the flag -fno-pic
- *			in order to avoid problems with gp register that may lead to a crash.
+ *            in order to avoid problems with gp register that may lead to a crash.
  *
 */
 STMOD_HANDLER sctrlHENSetStartModuleHandler(STMOD_HANDLER handler);
@@ -293,4 +302,66 @@ int sctrlKernelQuerySystemCall(void *func_addr);
 
 int sctrlKernelBootFrom(void);
 
+/**
+ * Patch module by offset
+ *
+ * @param modname - module name
+ * @param inst  - instruction
+ * @param offset - module patch offset
+ *
+ * @return < 0 on error
+ */
+int sctrlPatchModule(char *modname, u32 inst, u32 offset);
+
+/**
+ * Get module text address
+ *
+ * @param modname - module name
+ * 
+ * @return text address, or 0 if not found
+ */
+u32 sctrlModuleTextAddr(char *modname);
+
+/**
+ * Get sceInit module text address
+ *
+ * @note Only useful before sceInit exits
+ *
+ * @return text address, or 0 if not found
+ */
+u32 sctrlGetInitTextAddr(void);
+
+/**
+ * Set custom start module handler
+ * It can be used to replace a system module
+ *
+ * @note: func returns -1 to ignore the module and load the original module. Or new modid if replace is done.
+ */
+void sctrlSetCustomStartModule(int (*func)(int modid, SceSize argsize, void *argp, int *modstatus, SceKernelSMOption *opt));
+
+/**
+ * Loads a module on next reboot. Only kernel mode.
+ *
+ * @param module_after - The path of the module which is loaded after the module to be loaded.
+   The module passed to this function will be loaded just before that module.
+ * @param buf - The buffer containing the module - Don't deallocate this one. It has to reside in kernel memory.
+ * @param size - The size of the module
+ * @param flags - The modes in which the module should be loaded, one of BootLoadFlags
+ *
+ * @Example:
+ * sctrlHENLoadModuleOnReboot("/kd/usersystemlib.prx", module_buffer, module_size, BOOTLOAD_GAME | BOOTLOAD_POPS | BOOTLOAD_UMDEMU); 
+ *
+ * This will load the module contained in module_buffer just before /kd/usersystemlib.prx in the next reboot, if the mode of next reboot is game, pops or umdemu
+ *
+ * @Remarks: Don't use too early modules in first param like "/kd/init.prx" or "/kd/systemctrl.prx", or your module may not load properly
+ * Only one module will be loaded on reboot with this function. 
+ * If this function is called many times, only the last one will be considered.
+ * By making a module to load itself using this function, and calling 
+ * sctrlHENLoadModuleOnReboot on module_start, a prx can cause itself to be resident in the modes choosen by flags.
+ * If all flags are selected, the module will stay resident until a psp shutdown, or until sctrlHENLoadModuleOnReboot is not called.
+*/
+
+void sctrlHENLoadModuleOnReboot(char *module_after, void *buf, int size, int flags);
+
 #endif
+
