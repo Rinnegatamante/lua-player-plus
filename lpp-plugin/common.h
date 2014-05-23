6edc72a565747257c3f7ea21f575220fa9bcdb11
rinnegatamante@gmail.com <rinnegatamante@gmail.com@a7a990c0-74f6-f32d-1bde-ab2c4edc60e5>
2014-05-23 14:16:10 +0000

    Uploading PoC version of lpp.prx sourcecodes. The first LUA interpreter to create plugins for PSP/PSVITA.

---
+++ lpp-plugin/common.h
@@ -0,0 +1,21 @@
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspctrl.h>
+#include <pspdisplay.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#include "config.h"
+
+/*
+#include "ui.h"
+#include "../include/devhook.h"
+#include "../include/patch.h"
+
+#include "blit.h"
+#include "umd.h"
+
+#include "registry.h"
+*/
